e605bbfd5bc8a8ed07bd1870ca7ed00850803af0
Catalyst Admin <admin@catalysthealth.care>
2025-03-22 10:30:44 -0400

    Process Run Time Adjustments V2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@
 import express from 'express';
 import bulkImportRoute from './bulk-import-route';
 import testIntakeQRoutes from './test-intakeq';
-import testRoutes from './test';
 import webhookRoutes from './webhooks';
 import schedulingRoutes from './scheduling';
 import testWebhookRoutes from './test-webhook'; // Add this import
@@ -13,7 +12,6 @@ const router = express.Router();
 // Mount routes
 router.use('/bulk-import', bulkImportRoute);
 router.use('/test-intakeq', testIntakeQRoutes);
-router.use('/test', testRoutes);
 router.use('/webhooks', webhookRoutes);
 router.use('/scheduling', schedulingRoutes);
 router.use('/test-webhook', testWebhookRoutes); // Add this line
